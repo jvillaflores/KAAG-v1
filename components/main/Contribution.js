9778b5447918abe37f05dbcc422d643864c8cef7
jvillaflores <jvillaflores_180000001215@uic.edu.ph>
2021-09-30 17:59:07 +0800

    CONTRIBUTION BACK TO ORIGINAL
    Went back to contribution wherein the original IU

--- components/main/Contribution.js
+++ components/main/Contribution.js
@@ -8,16 +8,9 @@ import {
   Pressable,
   Image,
   TouchableOpacity,
-  ScrollView,
-  Modal
 } from "react-native";
 import { Camera } from "expo-camera";
 import * as ImagePicker from "expo-image-picker";
-
-import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
-import UploadList from "./UploadList";
-import UploadItem from "./UploadItem";
-
 
 export default function Contribution({ navigation }) {
   const [hasGalleryPermission, setHasGalleryPermission] = useState(null);
@@ -25,13 +18,6 @@ export default function Contribution({ navigation }) {
   const [camera, setCamera] = useState(null);
   const [image, setImage] = useState(null);
   const [type, setType] = useState(Camera.Constants.Type.back);
-
-  const [uploadModal, setUploadModal] = useState(false);
-  const closeUploadModal = () => {
-    setUploadModal(false);
-  };
- 
-  //const { width: winWidth, height: winHeight } = Dimensions.get('window');
 
   useEffect(() => {
     (async () => {
@@ -44,33 +30,12 @@ export default function Contribution({ navigation }) {
     })();
   }, []);
 
-
   const takePicture = async () => {
-    const { granted } = await ImagePicker.requestCameraPermissionsAsync();
-    if (granted) {
-      setUploadModal(false);
-      let data = await ImagePicker.launchCameraAsync({
-        mediaTypes: ImagePicker.MediaTypeOptions.Images,
-
-        aspect: [1, 1],
-        quality: 0.5,
-      });
-      if (!data.cancelled) {
-        //let newImages = [data, ...images];
-         setImage(data);
-        //setImage(newImages);
-      }
-    } else {
-      Alert.alert("Camera Permission Needed");
+    if (camera) {
+      const data = await camera.takePictureAsync(null);
+      setImage(data.uri);
     }
-    
-    
   };
-
-  const handleAddPhoto = () => {
-    setUploadModal(true);
-  };
-
 
   const pickImage = async () => {
     let result = await ImagePicker.launchImageLibraryAsync({
@@ -93,200 +58,52 @@ export default function Contribution({ navigation }) {
   if (hasCameraPermission === false || hasGalleryPermission === false) {
     return <Text>No access to camera</Text>;
   }
-
-  
   return (
-    <ScrollView 
-      style={{ flex: 1 }} 
-      contentContainerStyle={{ flex: 1 }}
+    <View style={styles.container}>
+      <Camera
+        ref={(ref) => setCamera(ref)}
+        style={styles.camera}
+        type={type}
+        ratio={"1:1"}
       >
-      <View style={styles.upload}>
-        <TextInput 
-            style={styles.text}
-            multiline = {true}
-            autoCorrect = {false}
-            blurOnSubmit = {true}
-            placeholder="What's happening?"/>
-            
-        
-        <View style={{ marginVertical: 10, flex: 1 }}>
-          <View style={ {flex: 1, alignItems: "center"}}>
-            {image && <UploadItem 
-                  item={image} 
-                  />}
-          </View>
-        </View>
-
-        {image && (
+        <View style={styles.buttonContainer}>
           <TouchableOpacity
-            //style={styles.cart__checkoutButton}
-            activeOpacity={0.7}
-            onPress={() => navigation.navigate("Save",{image})}
+            style={styles.button1}
+            onPress={() => {
+              setType(
+                type === Camera.Constants.Type.back
+                  ? Camera.Constants.Type.front
+                  : Camera.Constants.Type.back
+              );
+            }}
           >
-            <Text>Confirm</Text>
+            <Text style={styles.text}> Flip </Text>
           </TouchableOpacity>
-        )}
-
-        {!image && (
-          <View style={styles.cameraButton}> 
-          <View style={styles.imageButton}>
-            <TouchableOpacity
-                style={{
-                  ...styles.upload__containerInsideCol,
-                  borderRightColor: "lightgray",
-                  borderRightWidth: 1,
-                }}
-                onPress={takePicture}
-              >
-                <MaterialCommunityIcons name="camera" color="#8E2835" size={30} />
-                <Text style={styles.buttontext}>Camera</Text>
-              </TouchableOpacity>
-              <TouchableOpacity
-                style={styles.upload__containerInsideCol}
-                onPress={pickImage}
-              >
-                <MaterialCommunityIcons name="image" color="#8E2835" size={30} />
-                <Text style={styles.buttontext}>Gallery</Text>
-              </TouchableOpacity>
-            </View>
-          </View>
-        )}
-        
-      </View>
-    </ScrollView>
-    
-  )
+        </View>
+      </Camera>
+      <Button
+        title="Take Picture"
+        style={styles.button1}
+        onPress={() => takePicture()}
+      />
+      <Button
+        title="Pick Image from Gallery"
+        style={styles.button1}
+        onPress={() => pickImage()}
+      />
+      <Button
+        title="Post"
+        onPress={() => navigation.navigate("Save", { image })}
+      />
+      {image && <Image source={{ uri: image }} style={{ flex: 1 }} />}
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    justifyContent: 'center',
-    //backgroundColor: 'white'
+    height: 800,
   },
-  cameraButton:{
-    //position: "absolute",
-    bottom: 0,
-    width: 80,
-    height: 80,
-    // borderRadius: 20,
-    // borderColor: "#8E2835",
-    // borderWidth: 3,
-    alignSelf: "center",
-    flexDirection: "row",
-    justifyContent: "center",
-
-
-  },
-  imageButton:{
-    position: "absolute",
-    bottom: 20,
-    width: 80,
-    height: 50,
-    // borderRadius: 20,
-    // borderColor: "#8E2835",
-    // borderWidth: 3,
-    alignSelf: "center",
-    flexDirection: "row",
-    justifyContent: "center",
-  },
-  buttontext:{
-    color: "#8E2835",
-    fontSize: 10,
-  },
-  center:{
-    position: "absolute",
-    alignSelf: "center",
-    top: 12.5,
-  },
-  cameraContainer : {
-    flex: 1,
-    flexDirection: 'row',
-    
-  },
-  upload:{
-    flex: 1,
-    
-  },
-  upload__indicator:{
-    flex: 1,
-  },
-  upload__indicatorTitle:{
-    flex: 1,
-  },
-  upload__indicatorSemiTitle:{
-    flex: 1,
-  },
-  upload__containerOutside:{
-    position: "absolute",
-    bottom: 0,
-    width: '100%',
-    height: '16%',
-    backgroundColor: '#F2F2F2',
-    flexDirection: "row",
-    alignSelf: "center",
-    borderTopEndRadius: 25,
-    borderTopLeftRadius: 25,
-    shadowColor: '#8E2835',
-    
-    
-  },
-  upload__containerInside:{
-    flex:1,
-    flexDirection: "row",
-    alignItems: "center",
-    justifyContent: "space-evenly",
-  },
-
-  upload__containerInsideCol:{
-    margin: 1,
-    alignItems: "center",
-    width: 100,
-    height: 60,
-    justifyContent: "center",
-  },
-  camerafixedRatio: {
-    flex: 1,
-    aspectRatio: 1, 
-    backgroundColor: '#fff',
-  },
-  modalBackground:{
-    flex:1,
-    backgroundColor: 'rgba(0,0,0,0.5)',
-
-  },
-  
-  capture: {
-    position: "absolute",
-    bottom: 30,
-    width: 80,
-    height: 80,
-    borderRadius: 100,
-    borderColor: "#eee",
-    borderWidth: 6,
-    alignSelf: "center"
-
-  },
-  Flipbutton: {
-    position: "absolute",
-    alignSelf: "flex-end",
-    justifyContent: "center",
-    borderRadius: 4,
-    elevation: 3,
-    right: 25,
-    top: 15,
-    
-  },
-  ChooseImageButton: {
-    position: "absolute",
-    bottom: 10,
-    width: 80,
-    height: 80,
-    alignSelf: "flex-end"
-
-
-  },
-
   loginGroup: {
     flex: 1,
     alignContent: "center",
@@ -323,15 +140,28 @@ const styles = StyleSheet.create({
   button1: {
     marginBottom: 200,
   },
-  
+  button: {
+    alignSelf: "center",
+    justifyContent: "center",
+    paddingVertical: 12,
+    paddingHorizontal: 32,
+    borderRadius: 4,
+    elevation: 3,
+    left: -25,
+    width: "80%",
+    backgroundColor: "#8E2835",
+    borderTopLeftRadius: 10,
+    borderTopRightRadius: 10,
+    borderBottomRightRadius: 10,
+    borderBottomLeftRadius: 10,
+  },
+
   text: {
     fontSize: 20,
-    fontWeight: "normal",
+    fontWeight: "bold",
     lineHeight: 21,
     letterSpacing: 0.25,
-    color: "black",
-    padding: 15,
-    margin: 12
+    color: "white",
   },
   text1: {
     alignSelf: "center",
@@ -341,5 +171,4 @@ const styles = StyleSheet.create({
     letterSpacing: 0.25,
     color: "white",
   },
-
 });
